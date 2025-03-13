c858d91a2ac1b205e73be89a7e515273dee35648
Abhiman Ranaweera <abhiman.max@gmail.com>
2025-03-14 00:02:36 +0530

    feat: add query client provider

---
+++ src/QueryClientProvider.tsx
@@ -0,0 +1,15 @@
+// src/QueryClientProvider.tsx
+import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+const QueryClientProviderWrapper: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
+
+export default QueryClientProviderWrapper;

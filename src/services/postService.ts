9d53dec91e13cecd3b06599596f902332b4dcd81
Abhiman Ranaweera <abhiman.max@gmail.com>
2025-03-14 00:02:56 +0530

    feat: add post service

---
+++ src/services/postService.ts
@@ -0,0 +1,9 @@
+// src/services/postService.ts
+import axios from "axios";
+
+const BASE_URL = "https://jsonplaceholder.typicode.com";
+
+export const fetchPosts = async () => {
+  const response = await axios.get(`${BASE_URL}/posts`);
+  return response.data;
+};
